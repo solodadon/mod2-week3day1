6443ef9006c797254048add967b3b0c91f9022ba
solodadon <austinhutchins901@yahoo.com>
2025-02-24 08:59:20 -0600

    amen

---
+++ index.js
@@ -0,0 +1,16 @@
+
+    let myVariable = 3;
+    console.log(myVariable);
+    let betterVariable = 5;
+    console.log(betterVariable);
+    let yourNumber = myVariable + betterVariable;
+    console.log(3,5);
+    console.log(yourNumber);
+
+    // /+ addition
+    // - subtraction
+    // * multiplication
+    // / division
+    // % modulo
+    // commands / to comment out code
+    d
